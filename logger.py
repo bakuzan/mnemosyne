98b06eb347fe2fde00e5716b12ebf27b0e222db0
bakuzan <imannoyed4days@hotmail.co.uk>
2024-05-09 19:17:57 +0100

    Add powershell script to run the program; Try to get logger to behave with funny characters

--- logger.py
+++ logger.py
@@ -2,9 +2,13 @@ import logging, time
 
 logger = logging.getLogger(__name__)
 
-logger.setLevel(logging.INFO)
+# File Handler setup
 file_handler = logging.FileHandler(f'./logs/{time.time()}.log')
+formatter = logging.Formatter(u"%(message)s")
+file_handler.setFormatter(formatter)
 
+# Set logger values
+logger.setLevel(logging.INFO)
 logger.addHandler(file_handler)
 
 def get_logger():
